5663339d401821f1c60cb8939bc72a3994c986f3
unknown <pranityadav46@gmail.com>
2021-11-08 00:13:21 +0530

    Price Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "react-bootstrap";
+import Card from "./card.js";
+import {useSate} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let free= {
+    title: "FREE",
+    price: 0,
+    user: "Single User",
+    userEnabler: true,
+    storage: "5GB Storage",
+    storageEnabler: true,
+    publicProjects: "Unlimited Public Projects",
+    publicProjectsEnabler: true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects: "Unlimited Private Projects",
+    privateProjectsEnabler:false,
+    phoneSupport: "Dedicated Phone Support",
+    phoneSupportEnabler:false,
+    subdomain:"Free Subdomain",
+    subdomainEnabler:true,
+    reports:"Monthly Status Reports",
+    reportsEnabler:false,
+  }
+
+  let plus= {
+    title: "PLUS",
+    price: 9,
+    user: "5 Users",
+    userEnabler: true,
+    storage: "50GB Storage",
+    storageEnabler: true,
+    publicProjects: "Unlimited Public Projects",
+    publicProjectsEnabler: true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects: "Unlimited Private Projects",
+    privateProjectsEnabler:true,
+    phoneSupport: "Dedicated Phone Support",
+    phoneSupportEnabler:true,
+    subdomain:"Free Subdomain",
+    subdomainEnabler:true,
+    reports:"Monthly Status Reports",
+    reportsEnabler:false,
+  }
+
+  let pro= {
+    title: "PRO",
+    price: 49,
+    user: "Unlimited Users",
+    userEnabler: true,
+    storage: "150GB Storage",
+    storageEnabler: true,
+    publicProjects: "Unlimited Public Projects",
+    publicProjectsEnabler: true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects: "Unlimited Private Projects",
+    privateProjectsEnabler:true,
+    phoneSupport: "Dedicated Phone Support",
+    phoneSupportEnabler:true,
+    subdomainPrefix:"Unlimited",
+    subdomain:" Free Subdomains",
+    subdomainEnabler:true,
+    reports:"Monthly Status Reports",
+    reportsEnabler:true,
+  }
+  return <>
+      <section class="pricing py-5">
+        <div class="container">
+          <div class="row">
+            <Card type={free}/>
+            <Card type={plus}/>
+            <Card type={pro}/>
+          </div>
+        </div>
+      </section>
+    </>
 }
 
 export default App;
